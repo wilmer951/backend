68af16cf05dd5a213eee9cdd3f254a0ebeef6735
wpulidoe <wilmer-951@hotmail.com>
2025-09-30 15:26:23 -0500

    Modificacion de cors para  permitir acceso.

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,11 +8,16 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', '*'); // O la URL de tu frontend
+        if ($request->getMethod() === "OPTIONS") {
+            $response = response('', 200);
+        } else {
+            $response = $next($request);
+        }
+        
+        $response->headers->set('Access-Control-Allow-Origin', '*'); // o 'http://localhost:8080'
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-
+        
         return $response;
     }
 }
